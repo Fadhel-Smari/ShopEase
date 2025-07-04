f33e56c21f629747516735400c49281e51fb08b6
Fadhel-Smari <smari.fadhel@courrier.uqam.ca>
2025-07-03 22:17:13 -0400

    Point d’entrée React (index.js)

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+/**
+ * Point d’entrée principal de l’application React.
+ *
+ * Ce fichier :
+ * - importe les dépendances nécessaires,
+ * - sélectionne l’élément HTML racine,
+ * - rend le composant <App /> dans le DOM via ReactDOM.
+ */
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
